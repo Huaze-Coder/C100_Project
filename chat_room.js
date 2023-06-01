e3f3566da7ebb23ad1044b8239003d720b29c3c6
Huaze-Coder <111575739+Huaze-Coder@users.noreply.github.com>
2023-06-01 20:53:56 +0800

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -5,3 +5,13 @@ function logout(){
     localStorage.removeItem("user_name");
     window.location = "index.html";
 }
+
+function addroom(){
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({purpose : "adding room name"});
+
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "kwitter_page.html";
+  }
